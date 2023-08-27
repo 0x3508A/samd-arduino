584d806dae0ba6c931eb720ea55407c20ea37789
0x3508a <124862084+0x3508A@users.noreply.github.com>
2023-08-27 18:11:56 +0530

    Adding version for script

--- extras/transform.sh
+++ extras/transform.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 git fetch --tags
-VERSION="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
-git switch -c "$CORE_VERSION"
-echo "$VERSION" > VERSION.txt
-sed "s/%%VERSION%%/${VERSION}/" samd-arduino.json
-sed "s/%%VERSION%%/${VERSION}/" ../platform.txt
+VERSION="$(<VERSION.txt)"
+git switch -c "$VERSION"
+sed -i "s/%%VERSION%%/${VERSION}/" samd-arduino.json
+sed -i "s/%%VERSION%%/${VERSION}/" ../platform.txt
